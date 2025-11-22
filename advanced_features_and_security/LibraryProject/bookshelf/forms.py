bc5875c1f820c578c2742c8b23c7749a93de2c04
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-11-22 12:20:36 +0100

    third refactor for checker

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,7 +3,7 @@ from .models import Article
 
 
 
-class ArticleForm(ModelForm):
+class ExampleForm(ModelForm):
     class Meta:
         model= Article
         fields= ["title", "body"]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
-from .forms import ArticleForm
+from .forms import ExampleForm
 from .models import Article
 
 # Create your views here.
@@ -10,18 +10,18 @@ from .models import Article
 @permission_required("bookshelf.can_create", raise_exception= True)
 def create_article(request):
     if request.method == "POST":
-        form = ArticleForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             redirect("articles")
     else:
-        form = ArticleForm()
+        form = ExampleForm()
     
-    return render(request, "bookshelf/create-article.html", {"form": form})
+    return render(request, "bookshelf/form_example.html", {"form": form})
 
 @permission_required("bookshelf.can_view", raise_exception= True)
 def book_list(request):
-    articles = ArticleForm.objects.all()
+    articles = ExampleForm.objects.all()
     return render(request, "bookshelf/articles.html", {"articles": articles})
 
 @permission_required("bookshelf.can_edit", raise_exception= True)
@@ -29,12 +29,12 @@ def edit_article(request):
     article_id = request.POST.get("id")
     article = Article.objects.get(id= article_id)
     if request.method == "POST":
-        form = ArticleForm(request.POST, instance= article)
+        form = ExampleForm(request.POST, instance= article)
         if form.is_valid():
             form.save()
             redirect("articles")
     else:
-        form = ArticleForm(instance= article)
+        form = ExampleForm(instance= article)
     
     return render(request, "bookshelf/edit-article.html", {"form": form})
 
