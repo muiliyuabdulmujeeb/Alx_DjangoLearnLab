f0f592cae2254e7569616333c04b39f195a41188
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-12-06 21:13:17 +0100

    task4 correction1

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User, AnonymousUser
-from django.test import TestCase, RequestFactory
+from rest_framework.test import APITestCase
+from rest_framework import status
 from .models import Author, Book
 from .views import CreateView, UpdateView, DeleteView
 
 
-class BookTestCase(TestCase):
+class BookTestCase(APITestCase):
 
     def setUp(self):
-        self.factory = RequestFactory()
         self.user = User.objects.create_user(username="mockuser", email="mockuser@test.com", password="mock_password")
         self.author_one = Author.objects.create(name= "author_one")
         self.author_two = Author.objects.create(name= "author_two")
@@ -21,12 +21,10 @@ class BookTestCase(TestCase):
             "publication_year": 2020,
             "author": self.author_one
         }
-        request = self.factory.post("/books/create", data= data)
-        request.user = self.user
+        self.client.login(username="mockuser", password="mock_password")
+        response = self.client.post("/books/create", data= data)
 
-        response = CreateView.as_view()(request)
-
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     
     def test_update_book(self):
@@ -34,18 +32,14 @@ class BookTestCase(TestCase):
         to_update = {
             "publication_year": 2018
         }
+        self.client.login(username="mockuser", password="mock_password")
+        response = self.client.put("/books/update/1", data= to_update)
 
-        request = self.factory.put("/books/update/1", data= to_update)
-        request.user = self.user
-
-        response = UpdateView.as_view()(request)
-
-        self.assertEqual(response.content.publication_year, 2018)
+        self.assertEqual(response.data["publication_year"], 2018)
     
     def test_delete_book(self):
-        request = self.factory.delete("/books/delete/1")
-        request.user = self.user
 
-        response = DeleteView.as_view()(request)
+        self.client.login(username="mockuser", password="mock_password")
+        response = self.factory.delete("/books/delete/1")
 
-        self.assertEqual(response.content, None)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
