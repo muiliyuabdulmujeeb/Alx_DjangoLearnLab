ed5ace9176de80680c801b782d7718a0e9e4b3c2
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-12-06 12:44:11 +0100

    task2 correction 1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,9 @@ from .views import CreateView, DetailView, UpdateView, ListView, DeleteView
 
 
 urlpatterns = [
-    path("/books/", CreateView.as_view(), name="create_view"),
+    path("/books/create", CreateView.as_view(), name="create_view"),
     path("/books/", ListView.as_view(), name="list_view"),
     path("/books/<int: pk>", DetailView.as_view(), name="detail_view"),
-    path("/books/<int: pk>", UpdateView.as_view(), name="update_view"),
-    path("/books/<int: pk>", DeleteView.as_view(), name="delete_view"),
+    path("/books/update/<int: pk>", UpdateView.as_view(), name="update_view"),
+    path("/books/delete/<int: pk>", DeleteView.as_view(), name="delete_view"),
 ]
